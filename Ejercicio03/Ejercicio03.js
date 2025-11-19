ba5f201f99c9544352d591d085734411310cff97
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-11-19 17:50:36 -0500

    Finalizado Ejercicio 3

---
+++ Ejercicio03/Ejercicio03.js
@@ -0,0 +1,3 @@
+const imagen=document.querySelector('img');
+imagen.setAttribute("src","https://es.wikipedia.org/static/images/icons/wikipedia.png");
+imagen.setAttribute("alt","Logo de Wikipedia: Imagen reemplazada dinamicamente con JS");
